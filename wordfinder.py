177087506f0779329bb49e1478ed39cd31cef2d1
Lidan-Huang <huangld0314@gmail.com>
2022-07-12 14:30:00 -0700

    added random function -import random -method: random.choice()

--- wordfinder.py
+++ wordfinder.py
@@ -1,3 +1,5 @@
+import random
+
 class LineFinder:
     """Word Finder: finds random words from a dictionary.
 
@@ -6,11 +8,16 @@ class LineFinder:
     """
 
     def __init__(self, filepath):
+        """Create a generator to get and print words"""
         self.filepath = filepath
         self.words = self.read_file()
         print(f"{len(self.words)} words read")
 
     def read_file(self):
+        """Read file and return a list of words"""
         with open(self.filepath) as lines:
             return [line.strip() for line in lines]
         
+    def random(self):
+        """Get the random word from the words list"""
+        return random.choice(self.words)

743463ccc826ff9f81a3f385b127e642e34a6f41
Lidan-Huang <huangld0314@gmail.com>
2022-07-19 22:51:35 -0700

    added subclass specialwordfinder and passed the tests

--- wordfinder.py
+++ wordfinder.py
@@ -1,9 +1,9 @@
 import random
 
-class LineFinder:
+class WordFinder(object):
     """Word Finder: finds random words from a dictionary.
 
-    >>> wf = LineFinder("./words.txt")
+    >>> wf = WordFinder("./words.txt")
     5 words read
     """
 
@@ -21,3 +21,15 @@ class LineFinder:
     def random(self):
         """Get the random word from the words list"""
         return random.choice(self.words)
+
+
+class SpecialWordFinder(WordFinder):
+    # def __init__(self, filepath):
+    #     super().__init__(filepath)
+    #     self.words = self.read_file()
+
+    def read_file(self):
+        with open(self.filepath) as lines:
+            return [line.strip() for line in lines
+                    if (line.strip() != "" and not line.strip().startswith("#"))]
+           
